1abb5d78dfe0ad4df76a5a7da066dfa74e5bd076
sofia-papa <papa.sofia@outlook.it>
2021-08-26 09:50:19 +0100

    corretto la variabile

--- js/myscript.js
+++ js/myscript.js
@@ -1,18 +1,15 @@
-
-
-let numeroRichiesto = parseInt(prompt('Inserisci un numero'));
  
-for (let i = 1; i < 100; i++){
-    if ((numeroRichiesto % 3 == 0) && (numeroRichiesto % 5 == 0)) {
+for (let i = 1; i <= 100; i++){
+    if (( i % 3 == 0) && (i % 5 == 0)) {
         console.log("FizzBuzz")
     } 
-    else if (numeroRichiesto % 3 == 0) {
+    else if (i % 3 == 0) {
         console.log("Fizz")
     } 
-    else if (numeroRichiesto % 5 == 0){
+    else if (i % 5 == 0){
         console.log("Buzz")
     }
     else {
-       console.log ("non sei nè un multiplo di 3, nè un multiplo di 5")
+        console.log(i);
     }
 }
